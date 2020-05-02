d9c150adbe808dc43bf2b5d8a22b25db58ca8602
Ben Mezger <me@benmezger.nl>
2020-05-02 03:53:36 -0300

    Feat: Add deploy public script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+blog_head=$(git rev-parse HEAD)
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[1;31mDeploying commit $blog_head of 'blog' \n\033[1;0m"
+
+hugo --gc --minify
+
+# Add newly created/updated files
+cd public
+
+## Commit and push
+git add .
+git commit -F- <<EOF
+Automatic site rebuild of $(date)
+
+This is a rebuild of commit '$blog_head'
+Respository: github.com/benmezger/blog
+EOF
+
+#git push origin master
