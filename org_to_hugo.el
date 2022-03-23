085f0b265b41573067f36e4ebfbbe689869d408d
Ben Mezger <me@benmezger.nl>
2022-03-23 00:29:25 +0000

    chore: migrate to roam v2

--- org_to_hugo.el
+++ org_to_hugo.el
@@ -13,7 +13,8 @@
 (defun benmezger/org-roam-export-all ()
   "Re-exports all Org-roam files to Hugo markdown."
   (interactive)
-  (dolist (f (org-roam--list-all-files))
-    (with-current-buffer (find-file f)
+  (dolist (org-file (directory-files-recursively org-roam-directory "\.org$"))
+    (with-current-buffer (find-file org-file)
+      (message (format "[build] Exporting %s" org-file))
       (when (s-contains? "SETUPFILE" (buffer-string))
         (org-hugo-export-wim-to-md)))))
